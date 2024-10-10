6a5521f1b61577f6394cfab9e4abb0c2ebe14edd
Sarthak Tyagi <sarthaktyagi3846@gmail.com>
2024-10-10 17:55:02 +0530

    Add files via upload

---
+++ recipes.py
@@ -0,0 +1,58 @@
+# recipes.py
+
+recipes = [
+    {
+        "name": "Paneer Butter Masala",
+        "ingredients": ["paneer", "tomato", "butter", "cream", "spices"],
+        "diet": "bulking",
+        "calories": 450
+    },
+    {
+        "name": "Aloo Paratha",
+        "ingredients": ["potato", "flour", "butter", "spices"],
+        "diet": "cutting",
+        "calories": 300
+    },
+    {
+        "name": "Masala Dosa",
+        "ingredients": ["rice", "lentils", "potato", "curry leaves", "spices"],
+        "diet": "fat loss",
+        "calories": 350
+    },
+    {
+        "name": "Dal Tadka",
+        "ingredients": ["lentils", "ghee", "tomato", "onion", "spices"],
+        "diet": "low-fat",
+        "calories": 250
+    },
+    {
+        "name": "Chicken Biryani",
+        "ingredients": ["rice", "chicken", "yogurt", "onion", "spices"],
+        "diet": "bulking",
+        "calories": 600
+    },
+    {
+        "name": "Palak Paneer",
+        "ingredients": ["spinach", "paneer", "cream", "onion", "spices"],
+        "diet": "cutting",
+        "calories": 350
+    },
+    {
+        "name": "Roti with Sabzi",
+        "ingredients": ["flour", "vegetables", "oil", "spices"],
+        "diet": "low-fat",
+        "calories": 300
+    },
+    {
+        "name": "Butter Chicken",
+        "ingredients": ["chicken", "butter", "tomato", "cream", "spices"],
+        "diet": "bulking",
+        "calories": 550
+    },
+    {
+        "name": "Idli Sambar",
+        "ingredients": ["rice", "urad dal", "tamarind", "vegetables", "spices"],
+        "diet": "fat loss",
+        "calories": 200
+    }
+]
